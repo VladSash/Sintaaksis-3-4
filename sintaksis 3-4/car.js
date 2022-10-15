38084faafc16e4b2fae7893fe519d94fc5c972c1
VladSash <115373009+VladSash@users.noreply.github.com>
2022-10-15 18:36:33 +0300

    Add files via upload

---
+++ sintaksis 3-4/car.js
@@ -0,0 +1,12 @@
+const carName="Мерседес"
+alert(carName)
+
+let maxCarSpeed="250км/ч"
+console.log('У машины изменилась скорость, ', maxCarSpeed="190")
+console.log('У машины изменилась скорость, ', maxCarSpeed="160")
+alert(maxCarSpeed)
+
+let carOwner="Василий"
+console.log('У машины изменился владелец, ', carOwner="Олег")
+console.log('У машины изменился владелец, ', carOwner="Джорж")
+alert(carOwner)
